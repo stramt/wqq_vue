b6a1fd4e18b4486070cfb8796313c1aa92270106
stramt <stramt0204@163.com>
2021-03-29 10:56:48 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ const routes = [
     path: "/about",
     name: "About",
     // route level code-splitting
+    //初始化
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
